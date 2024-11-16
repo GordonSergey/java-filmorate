507182da73844a3bc36b17f7d644bf0ef86339ad
Sergey Gordon <mrgordon1111@gmail.com>
2024-11-16 15:09:43 +0100

    ok

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,13 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
-    // Конструктор с сообщением об ошибке
     public ValidationException(String message) {
         super(message);
-    }
-
-    // Конструктор с сообщением и причиной ошибки
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
